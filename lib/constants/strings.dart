651dc05f4986137db0df4cb85bbc2bff2b19effa
Sameer Ahmed <ahmedkhansameer50@gmail.com>
2021-06-27 05:21:16 +0530

    Update strings.dart

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -6,7 +6,7 @@ class Strings {
   static const String me = ' Me';
   static const String portfoli = 'Portfoli';
   static const String o = 'o';
-  static const String headline = 'I\'am Zubair Rehman, Mobile App Developer from Islamabad, Pakistan';
+  static const String headline = 'I\ am Zubair Rehman, Mobile App Developer from Islamabad, Pakistan';
   static const String summary = 'Focused professional having excellent technical and communication skills, and offering 6 years of experience in Computer industry. Proficient at designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
   static const String experience = 'Experience';
   static const String skills_i_have = 'What Skill I Have';
